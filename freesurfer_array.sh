e71a71c09f3ec75df4d968a75205f11c0aa36918
NickRiccardi <64374486+NickRiccardi@users.noreply.github.com>
2020-04-30 15:25:06 -0400

    Update freesurfer_array.sh

--- freesurfer_array.sh
+++ freesurfer_array.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
+### set job name
 #SBATCH --job-name=freesurfer_array
+### set number of nodes (more is better for FreeSurfer)
 #SBATCH -n 12
 #SBATCH -N 1
+### output files named after array ID
 #SBATCH --output FS_%a.out
 #SBATCH --error FS_%a.err
+### set subjects that you want to run
 #SBATCH --array=1001-1056
 #SBATCH -p soph
 
@@ -12,5 +16,8 @@ module load afni
 module load netpbm
 module load freesurfer/2020-beta
 
+### cd into each subject's data directory
 cd ${SLURM_ARRAY_TASK_ID}
+### create surfaces and put output in subject's data directory
+### openmp number should match the number of nodes requested above
 recon-all -s ${SLURM_ARRAY_TASK_ID}_FS -i T1.nii -sd /home/riccardn/data/ABC/${SLURM_ARRAY_TASK_ID} -all -parallel -openmp 12

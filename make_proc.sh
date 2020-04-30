7893d39e9c926e8b735149d95f5c42cac25a128e
NickRiccardi <64374486+NickRiccardi@users.noreply.github.com>
2020-04-30 15:28:16 -0400

    Update make_proc.sh

--- make_proc.sh
+++ make_proc.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+### set job name
 #SBATCH --job-name=make_proc
+### request number of nodes (should always be 1 for this script)
 #SBATCH -n 1
 #SBATCH -N 1
 #SBATCH --output make_proc.out
@@ -10,6 +12,8 @@
 module load afni
 module load netpbm
 module load python2.7/anaconda/5.0.0
-	
+
+### generate AFNI proc script for every subject specified within these cmd.* scripts
+### comment out *fam or *pass if you're only focusing on one of the tasks
 tcsh cmd.ap.ABC_fam_make_proc
 tcsh cmd.ap.ABC_pass_make_proc
